d1745e93b43c5f398b42e30cd02324c8f6d70bf1
Tyler Haas <haasdevelopment@gmail.com>
2018-01-15 21:12:33 -0700

    added styles

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,15 +1,45 @@
 import React from 'react'
-import { View, Text, TextInput, TouchableHighlight } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from 'react-native'
 
 export default ({ btnHandler, inputHandler, inputText }) => (
   <View>
     <TextInput
-      style={{ height: 100, width: 50 }}
       onChangeText={inputHandler}
+      placeholder="What do you want TODO?"
+      style={styles.input}
       value={inputText}
     />
-    <TouchableHighlight onPress={btnHandler} title="Add Todo">
-      <Text>Add Todo</Text>
+    <TouchableHighlight
+      style={styles.button}
+      onPress={btnHandler}
+      title="Add Todo"
+    >
+      <Text style={styles.buttonText}>Add Todo</Text>
     </TouchableHighlight>
   </View>
 )
+
+const height = 100
+
+const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderRadius: 3,
+    backgroundColor: 'lightblue',
+    height,
+  },
+  buttonText: {
+    textAlign: 'center',
+    lineHeight: height - 10,
+  },
+  input: {
+    height: 50,
+    margin: 50,
+  },
+})
